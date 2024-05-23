971085f65d9695fdf0f6e8f5dc1dd5cb91c7654b
manhtiendev <manhtiendev1307@gmail.com>
2024-05-23 11:35:50 +0700

    Refactor Project Structure

---
+++ src/pages/Boards/BoardBar/index.jsx
@@ -0,0 +1,18 @@
+import Box from '@mui/material/Box';
+
+function BoardBar() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: 'primary.dark',
+        width: '100%',
+        height: (theme) => theme.custom.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      Board Bar
+    </Box>
+  );
+}
+export default BoardBar;
