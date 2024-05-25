2992d60a850157c8bac35a131be1d284460abe31
manhtiendev <manhtiendev1307@gmail.com>
2024-05-25 19:24:03 +0700

    Refactor code

---
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -0,0 +1,55 @@
+import Box from '@mui/material/Box';
+import Column from './Column/Column';
+import Button from '@mui/material/Button';
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
+
+function ListColumns() {
+  return (
+    <Box
+      sx={{
+        bgcolor: 'inherit',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        overflowX: 'auto',
+        overflowY: 'hidden',
+        '&::-webkit-scrollbar-track': {
+          m: 2,
+        },
+      }}
+    >
+      <Column />
+      <Column />
+      <Column />
+
+      {/* Button Add Column */}
+      <Box
+        sx={{
+          minWidth: '200px',
+          maxWidth: '200px',
+          mx: 2,
+          borderRadius: '6px',
+          height: 'fit-content',
+          bgcolor: '#ffffff3d',
+          '&:hover': {
+            opacity: 0.8,
+          },
+        }}
+      >
+        <Button
+          sx={{
+            color: '#fff',
+            width: '100%',
+            justifyContent: 'flex-start',
+            pl: 2.5,
+            py: 1,
+          }}
+          startIcon={<NoteAddIcon />}
+        >
+          Add new column
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+export default ListColumns;
