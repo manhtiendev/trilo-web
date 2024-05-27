9ae15c9e5d38bab6a489b40c81fa08bb5dd2ac07
manhtiendev <manhtiendev1307@gmail.com>
2024-05-27 15:54:27 +0700

    Dnd Columns

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,20 +1,82 @@
+import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import ListColumns from './ListColumns/ListColumns';
 import { mapOrder } from '~/utils/sorts';
+import {
+  DndContext,
+  // PointerSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import { arrayMove } from '~/utils/arrayMove';
+// import { arrayMove } from '@dnd-kit/sortable';
 
 export default function BoardContent({ board }) {
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id');
+  const [orderedColumns, setorderedColumns] = useState([]);
+
+  useEffect(() => {
+    setorderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'));
+  }, [board]);
+
+  const handleDragEnd = (e) => {
+    const { active, over } = e;
+
+    if (!over) return;
+
+    if (active.id !== over.id) {
+      const oldIndex = orderedColumns.findIndex((c) => c._id === active.id);
+      const newIndex = orderedColumns.findIndex((c) => c._id === over.id);
+
+      setorderedColumns(arrayMove(orderedColumns, oldIndex, newIndex));
+    }
+  };
+
+  // Sensor
+
+  // If using the default PointerSensor,
+  // must incorporate CSS touch-action: 'none' in the drag and drop element(Columns)
+
+  // const pointerSensor = useSensor(PointerSensor, {
+  //   // Require the mouse to move by 10 pixels before activating
+  //   activationConstraint: {
+  //     distance: 10,
+  //   },
+  // });
+
+  // const sensors = useSensors(pointerSensor);
+
+  //PointerSensor bug on mobile, use MouseSensor & TouchSensor
+
+  const mouseSensor = useSensor(MouseSensor, {
+    // Require the mouse to move by 10 pixels before activating
+    activationConstraint: {
+      distance: 10,
+    },
+  });
+  const touchSensor = useSensor(TouchSensor, {
+    // Hold 250ms and
+    activationConstraint: {
+      delay: 250,
+      tolerance: 500,
+    },
+  });
+  const sensors = useSensors(mouseSensor, touchSensor);
+
   return (
-    <Box
-      sx={{
-        bgcolor: (theme) =>
-          theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
-        width: '100%',
-        height: (theme) => theme.custom.boardContentHeight,
-        p: '10px 0',
-      }}
-    >
-      <ListColumns columns={orderedColumns} />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box
+        sx={{
+          bgcolor: (theme) =>
+            theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
+          width: '100%',
+          height: (theme) => theme.custom.boardContentHeight,
+          p: '10px 0',
+        }}
+      >
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
   );
 }
--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -2,54 +2,63 @@ import Box from '@mui/material/Box';
 import Column from './Column/Column';
 import Button from '@mui/material/Button';
 import NoteAddIcon from '@mui/icons-material/NoteAdd';
+import {
+  SortableContext,
+  horizontalListSortingStrategy,
+} from '@dnd-kit/sortable';
 
 function ListColumns({ columns }) {
   return (
-    <Box
-      sx={{
-        bgcolor: 'inherit',
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        overflowX: 'auto',
-        overflowY: 'hidden',
-        '&::-webkit-scrollbar-track': {
-          m: 2,
-        },
-      }}
+    <SortableContext
+      items={columns?.map((col) => col._id)}
+      strategy={horizontalListSortingStrategy}
     >
-      {columns?.map((column) => (
-        <Column key={column._id} column={column} />
-      ))}
-
-      {/* Button Add Column */}
       <Box
         sx={{
-          minWidth: '200px',
-          maxWidth: '200px',
-          mx: 2,
-          borderRadius: '6px',
-          height: 'fit-content',
-          bgcolor: '#ffffff3d',
-          '&:hover': {
-            opacity: 0.8,
+          bgcolor: 'inherit',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          overflowX: 'auto',
+          overflowY: 'hidden',
+          '&::-webkit-scrollbar-track': {
+            m: 2,
           },
         }}
       >
-        <Button
+        {columns?.map((column) => (
+          <Column key={column._id} column={column} />
+        ))}
+
+        {/* Button Add Column */}
+        <Box
           sx={{
-            color: '#fff',
-            width: '100%',
-            justifyContent: 'flex-start',
-            pl: 2.5,
-            py: 1,
+            minWidth: '200px',
+            maxWidth: '200px',
+            mx: 2,
+            borderRadius: '6px',
+            height: 'fit-content',
+            bgcolor: '#ffffff3d',
+            '&:hover': {
+              opacity: 0.8,
+            },
           }}
-          startIcon={<NoteAddIcon />}
         >
-          Add new column
-        </Button>
+          <Button
+            sx={{
+              color: '#fff',
+              width: '100%',
+              justifyContent: 'flex-start',
+              pl: 2.5,
+              py: 1,
+            }}
+            startIcon={<NoteAddIcon />}
+          >
+            Add new column
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </SortableContext>
   );
 }
 export default ListColumns;
