b5bd981a2157071335052f01b7bf92af5833db5b
manhtiendev <manhtiendev1307@gmail.com>
2024-06-07 08:37:24 +0700

    Fix bug dnd card to empty col

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -2,3 +2,12 @@ export const capitalizeFirstLetter = (val) => {
   if (!val) return '';
   return `${val.charAt(0).toUpperCase()}${val.slice(1)}`;
 };
+
+export const generatePlaceholderCard = (col) => {
+  return {
+    _id: `${col._id}-placeholder-card`,
+    boardId: col.boardId,
+    columnId: col._id,
+    FE_PlaceholderCard: true,
+  };
+};
