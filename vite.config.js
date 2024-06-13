ad6d611ded14829f77261f207a1a60e22c04a6c7
manhtiendev <manhtiendev1307@gmail.com>
2024-06-13 21:54:40 +0700

    Fix bug module

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '~', replacement: '/src' }],
   },
-  server: {
-    open: true,
-    port: 5173,
-  },
+  // server: {
+  //   open: true,
+  //   port: 5173,
+  // },
 });
