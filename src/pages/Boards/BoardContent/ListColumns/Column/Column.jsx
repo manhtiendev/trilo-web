dacbc00a4141b1ea426e784379404a8d47b68842
manhtiendev <manhtiendev1307@gmail.com>
2024-06-20 09:01:56 +0700

    Delete Col

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -1,31 +1,32 @@
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ContentCut from "@mui/icons-material/ContentCut";
-import Cloud from "@mui/icons-material/Cloud";
-import Divider from "@mui/material/Divider";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Button from "@mui/material/Button";
-import { useState } from "react";
-import Tooltip from "@mui/material/Tooltip";
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import ContentCopy from "@mui/icons-material/ContentCopy";
-import AddCardIcon from "@mui/icons-material/AddCard";
-import DragHandleIcon from "@mui/icons-material/DragHandle";
-import ContentPaste from "@mui/icons-material/ContentPaste";
-import ListCards from "./ListCards/ListCards";
-import { useSortable } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
-import TextField from "@mui/material/TextField";
-import CloseIcon from "@mui/icons-material/Close";
-import { toast } from "react-toastify";
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ContentCut from '@mui/icons-material/ContentCut';
+import Cloud from '@mui/icons-material/Cloud';
+import Divider from '@mui/material/Divider';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import Tooltip from '@mui/material/Tooltip';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import ContentCopy from '@mui/icons-material/ContentCopy';
+import AddCardIcon from '@mui/icons-material/AddCard';
+import DragHandleIcon from '@mui/icons-material/DragHandle';
+import ContentPaste from '@mui/icons-material/ContentPaste';
+import ListCards from './ListCards/ListCards';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import TextField from '@mui/material/TextField';
+import CloseIcon from '@mui/icons-material/Close';
+import { toast } from 'react-toastify';
+import { useConfirm } from 'material-ui-confirm';
 
-function Column({ column, createNewCard }) {
+function Column({ column, createNewCard, deleteColumn }) {
   const [openNewCard, setOpenNewCard] = useState(false);
-  const [newCardTitle, setNewCardTitle] = useState("");
+  const [newCardTitle, setNewCardTitle] = useState('');
   // Dropdown menu
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
@@ -49,7 +50,7 @@ function Column({ column, createNewCard }) {
     // touchAction: 'none',
     transform: CSS.Translate.toString(transform),
     transition,
-    height: "100%",
+    height: '100%',
     opacity: isDragging ? 0.5 : undefined,
   };
 
@@ -59,9 +60,9 @@ function Column({ column, createNewCard }) {
 
   const addNewCard = async () => {
     if (!newCardTitle) {
-      toast.error("Please enter Card Title!", {
-        position: "bottom-right",
-        theme: "colored",
+      toast.error('Please enter Card Title!', {
+        position: 'bottom-right',
+        theme: 'colored',
         autoClose: 2000,
       });
       return;
@@ -74,7 +75,28 @@ function Column({ column, createNewCard }) {
     createNewCard(newCardData);
 
     toggleOpenNewCard();
-    setNewCardTitle("");
+    setNewCardTitle('');
+  };
+
+  const confirm = useConfirm();
+  const handleDeleteColumn = () => {
+    confirm({
+      title: 'Delete Column?',
+      description: `This action will permanently delete your Column and its Cards! Are you sure? Type 'columns' to confirm your action!`,
+      confirmationText: 'Confirm',
+      cancellationText: 'Cancel',
+      confirmationKeyword: 'columns',
+      confirmationKeywordTextFieldProps: {
+        sx: {
+          mt: 1,
+          fontSize: '16px',
+        },
+      },
+    })
+      .then(() => {
+        deleteColumn(column._id);
+      })
+      .catch(() => {});
   };
 
   return (
@@ -84,10 +106,10 @@ function Column({ column, createNewCard }) {
         sx={{
           minWidth: 300,
           maxWidth: 300,
-          bgcolor: (theme) => (theme.palette.mode === "dark" ? "#333643" : "#ebecf0"),
+          bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
           ml: 2,
-          borderRadius: "6px",
-          height: "fit-content",
+          borderRadius: '6px',
+          height: 'fit-content',
           maxHeight: (theme) => `calc(${theme.custom.boardContentHeight} - ${theme.spacing(5)})`,
         }}
       >
@@ -96,74 +118,95 @@ function Column({ column, createNewCard }) {
           sx={{
             height: (theme) => theme.custom.columnHeaderHeight,
             p: 2,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
           }}
         >
-          <Typography variant="h6" sx={{ fontWeight: "bold", fontSize: "1rem", cursor: "pointer" }}>
+          <Typography variant='h6' sx={{ fontWeight: 'bold', fontSize: '1rem', cursor: 'pointer' }}>
             {column?.title}
           </Typography>
           <Box>
-            <Tooltip title="More options">
+            <Tooltip title='More options'>
               <ExpandMoreIcon
                 sx={{
-                  color: "text.primary",
-                  cursor: "pointer",
-                  "&:hover": {
-                    backgroundColor: "#C9D8E6",
+                  color: 'text.primary',
+                  cursor: 'pointer',
+                  '&:hover': {
+                    backgroundColor: '#C9D8E6',
                   },
                 }}
-                id="basic-column-dropdown"
-                aria-controls={open ? "basic-menu-column-dropdown" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
+                id='basic-column-dropdown'
+                aria-controls={open ? 'basic-menu-column-dropdown' : undefined}
+                aria-haspopup='true'
+                aria-expanded={open ? 'true' : undefined}
                 onClick={handleClick}
               />
             </Tooltip>
             <Menu
-              id="basic-menu-column-dropdown"
+              id='basic-menu-column-dropdown'
               anchorEl={anchorEl}
               open={open}
               onClose={handleClose}
+              onClick={handleClose}
               MenuListProps={{
-                "aria-labelledby": "basic-column-dropdown",
+                'aria-labelledby': 'basic-column-dropdown',
               }}
             >
-              <MenuItem>
+              <MenuItem
+                onClick={toggleOpenNewCard}
+                sx={{
+                  '&:hover': {
+                    color: 'success.light',
+                    '& .add-card-icon': {
+                      color: 'success.light',
+                    },
+                  },
+                }}
+              >
                 <ListItemIcon>
-                  <AddCardIcon fontSize="small" />
+                  <AddCardIcon className='add-card-icon' fontSize='small' />
                 </ListItemIcon>
                 <ListItemText>Add new card</ListItemText>
               </MenuItem>
               <MenuItem>
                 <ListItemIcon>
-                  <ContentCut fontSize="small" />
+                  <ContentCut fontSize='small' />
                 </ListItemIcon>
                 <ListItemText>Cut</ListItemText>
               </MenuItem>
               <MenuItem>
                 <ListItemIcon>
-                  <ContentCopy fontSize="small" />
+                  <ContentCopy fontSize='small' />
                 </ListItemIcon>
                 <ListItemText>Copy</ListItemText>
               </MenuItem>
               <MenuItem>
                 <ListItemIcon>
-                  <ContentPaste fontSize="small" />
+                  <ContentPaste fontSize='small' />
                 </ListItemIcon>
                 <ListItemText>Paste</ListItemText>
               </MenuItem>
               <Divider />
-              <MenuItem>
+              <MenuItem
+                onClick={handleDeleteColumn}
+                sx={{
+                  '&:hover': {
+                    color: 'warning.dark',
+                    '& .delete-forever-icon': {
+                      color: 'warning.dark',
+                    },
+                  },
+                }}
+              >
                 <ListItemIcon>
-                  <DeleteForeverIcon fontSize="small" />
+                  <DeleteForeverIcon fontSize='small' className='delete-forever-icon' />
                 </ListItemIcon>
-                <ListItemText>Remove this column</ListItemText>
+                <ListItemText>Delete this column</ListItemText>
               </MenuItem>
               <MenuItem>
                 <ListItemIcon>
-                  <Cloud fontSize="small" />
+                  <Cloud fontSize='small' />
                 </ListItemIcon>
                 <ListItemText>Archive this column</ListItemText>
               </MenuItem>
@@ -184,94 +227,94 @@ function Column({ column, createNewCard }) {
           {!openNewCard ? (
             <Box
               sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-                height: "100%",
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                height: '100%',
               }}
             >
               <Button startIcon={<AddCardIcon />} onClick={toggleOpenNewCard}>
                 Add new card
               </Button>
-              <Tooltip title="Drag to move">
-                <DragHandleIcon sx={{ cursor: "pointer" }} />
+              <Tooltip title='Drag to move'>
+                <DragHandleIcon sx={{ cursor: 'pointer' }} />
               </Tooltip>
             </Box>
           ) : (
             <Box
               sx={{
-                height: "100%",
-                display: "flex",
-                alignItems: "center",
-                transition: "0.5s",
+                height: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                transition: '0.5s',
                 gap: 1,
               }}
             >
               <TextField
-                label="Enter card title..."
-                type="text"
-                size="small"
-                variant="outlined"
+                label='Enter card title...'
+                type='text'
+                size='small'
+                variant='outlined'
                 autoFocus
-                data-no-dnd="true"
+                data-no-dnd='true'
                 value={newCardTitle}
                 onChange={(e) => {
                   setNewCardTitle(e.target.value);
                 }}
                 sx={{
-                  "& label": { color: "text.primary" },
-                  "& input": {
+                  '& label': { color: 'text.primary' },
+                  '& input': {
                     color: (theme) => theme.palette.primary.main,
-                    bgcolor: (theme) => (theme.palette.mode === "dark" ? "#333643" : "white"),
+                    bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : 'white'),
                   },
-                  "& label.Mui-focused": { color: (theme) => theme.palette.primary.main },
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
+                  '& label.Mui-focused': { color: (theme) => theme.palette.primary.main },
+                  '& .MuiOutlinedInput-root': {
+                    '& fieldset': {
                       borderColor: (theme) => theme.palette.primary.main,
                     },
-                    "&:hover fieldset": {
+                    '&:hover fieldset': {
                       borderColor: (theme) => theme.palette.primary.main,
                     },
-                    "&.Mui-focused fieldset": {
+                    '&.Mui-focused fieldset': {
                       borderColor: (theme) => theme.palette.primary.main,
                     },
                   },
-                  "& .MuiOutlinedInput-input": {
+                  '& .MuiOutlinedInput-input': {
                     borderRadius: 1,
                   },
                 }}
               />
               <Box
                 sx={{
-                  display: "flex",
-                  alignItems: "center",
+                  display: 'flex',
+                  alignItems: 'center',
                   gap: 1,
                 }}
               >
-                <Tooltip title="Add new card">
+                <Tooltip title='Add new card'>
                   <Button
-                    data-no-dnd="true"
+                    data-no-dnd='true'
                     onClick={addNewCard}
-                    variant="contained"
-                    color="success"
-                    size="small"
+                    variant='contained'
+                    color='success'
+                    size='small'
                     sx={{
-                      boxShadow: "none",
-                      border: "0.5px solid",
+                      boxShadow: 'none',
+                      border: '0.5px solid',
                       borderColor: (theme) => theme.palette.success.main,
-                      "&:hover": { bgcolor: (theme) => theme.palette.success.main },
+                      '&:hover': { bgcolor: (theme) => theme.palette.success.main },
                     }}
                   >
                     Add
                   </Button>
                 </Tooltip>
-                <Tooltip title="Close">
+                <Tooltip title='Close'>
                   <CloseIcon
-                    data-no-dnd="true"
-                    fontSize="small"
+                    data-no-dnd='true'
+                    fontSize='small'
                     sx={{
                       color: (theme) => theme.palette.success.main,
-                      cursor: "pointer",
+                      cursor: 'pointer',
                     }}
                     onClick={() => {
                       toggleOpenNewCard();
--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -1,26 +1,26 @@
-import Box from "@mui/material/Box";
-import Column from "./Column/Column";
-import Button from "@mui/material/Button";
-import NoteAddIcon from "@mui/icons-material/NoteAdd";
-import { SortableContext, horizontalListSortingStrategy } from "@dnd-kit/sortable";
-import { useState } from "react";
-import TextField from "@mui/material/TextField";
-import CloseIcon from "@mui/icons-material/Close";
-import { Tooltip } from "@mui/material";
-import { toast } from "react-toastify";
+import Box from '@mui/material/Box';
+import Column from './Column/Column';
+import Button from '@mui/material/Button';
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
+import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import CloseIcon from '@mui/icons-material/Close';
+import { Tooltip } from '@mui/material';
+import { toast } from 'react-toastify';
 
-function ListColumns({ columns, createNewColumn, createNewCard }) {
+function ListColumns({ columns, createNewColumn, createNewCard, deleteColumn }) {
   const [openNewColumn, setOpenNewColumn] = useState(false);
-  const [newColumnTitle, setNewColumnTitle] = useState("");
+  const [newColumnTitle, setNewColumnTitle] = useState('');
   const toggleOpenNewColumn = () => {
     setOpenNewColumn(!openNewColumn);
   };
 
   const addNewColumn = async () => {
     if (!newColumnTitle) {
-      toast.error("Please enter Column Title!", {
-        position: "bottom-left",
-        theme: "colored",
+      toast.error('Please enter Column Title!', {
+        position: 'bottom-left',
+        theme: 'colored',
         autoClose: 2000,
       });
       return;
@@ -33,7 +33,7 @@ function ListColumns({ columns, createNewColumn, createNewCard }) {
     createNewColumn(newColumnData);
 
     toggleOpenNewColumn();
-    setNewColumnTitle("");
+    setNewColumnTitle('');
   };
 
   return (
@@ -43,19 +43,24 @@ function ListColumns({ columns, createNewColumn, createNewCard }) {
     >
       <Box
         sx={{
-          bgcolor: "inherit",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          overflowX: "auto",
-          overflowY: "hidden",
-          "&::-webkit-scrollbar-track": {
+          bgcolor: 'inherit',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          overflowX: 'auto',
+          overflowY: 'hidden',
+          '&::-webkit-scrollbar-track': {
             m: 2,
           },
         }}
       >
         {columns?.map((column) => (
-          <Column key={column._id} column={column} createNewCard={createNewCard} />
+          <Column
+            key={column._id}
+            column={column}
+            createNewCard={createNewCard}
+            deleteColumn={deleteColumn}
+          />
         ))}
 
         {/* Button Add Column */}
@@ -63,23 +68,23 @@ function ListColumns({ columns, createNewColumn, createNewCard }) {
           <Box
             onClick={toggleOpenNewColumn}
             sx={{
-              minWidth: "250px",
-              maxWidth: "250px",
+              minWidth: '250px',
+              maxWidth: '250px',
               mx: 2,
-              borderRadius: "6px",
-              height: "fit-content",
-              bgcolor: "#ffffff3d",
-              "&:hover": {
+              borderRadius: '6px',
+              height: 'fit-content',
+              bgcolor: '#ffffff3d',
+              '&:hover': {
                 opacity: 0.8,
               },
-              transition: "0.2s",
+              transition: '0.2s',
             }}
           >
             <Button
               sx={{
-                color: "#fff",
-                width: "100%",
-                justifyContent: "flex-start",
+                color: '#fff',
+                width: '100%',
+                justifyContent: 'flex-start',
                 pl: 2.5,
                 py: 1,
               }}
@@ -91,76 +96,76 @@ function ListColumns({ columns, createNewColumn, createNewCard }) {
         ) : (
           <Box
             sx={{
-              maxWidth: "250px",
-              minWidth: "250px",
+              maxWidth: '250px',
+              minWidth: '250px',
               mx: 2,
               p: 1,
-              borderRadius: "6px",
-              height: "fit-content",
-              bgcolor: "#ffffff3d",
-              display: "flex",
-              flexDirection: "column",
+              borderRadius: '6px',
+              height: 'fit-content',
+              bgcolor: '#ffffff3d',
+              display: 'flex',
+              flexDirection: 'column',
               gap: 1,
-              transition: "0.2s",
+              transition: '0.2s',
             }}
           >
             <TextField
-              label="Enter column title..."
-              type="text"
-              size="small"
-              variant="outlined"
+              label='Enter column title...'
+              type='text'
+              size='small'
+              variant='outlined'
               autoFocus
               value={newColumnTitle}
               onChange={(e) => {
                 setNewColumnTitle(e.target.value);
               }}
               sx={{
-                "& label": { color: "white" },
-                "& input": { color: "white" },
-                "& label.Mui-focused": { color: "white" },
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderColor: "white",
+                '& label': { color: 'white' },
+                '& input': { color: 'white' },
+                '& label.Mui-focused': { color: 'white' },
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': {
+                    borderColor: 'white',
                   },
-                  "&:hover fieldset": {
-                    borderColor: "#bdc3c7",
+                  '&:hover fieldset': {
+                    borderColor: '#bdc3c7',
                   },
-                  "&.Mui-focused fieldset": {
-                    borderColor: "white",
+                  '&.Mui-focused fieldset': {
+                    borderColor: 'white',
                   },
                 },
               }}
             />
             <Box
               sx={{
-                display: "flex",
-                alignItems: "center",
+                display: 'flex',
+                alignItems: 'center',
                 gap: 1,
               }}
             >
-              <Tooltip title="Add new column">
+              <Tooltip title='Add new column'>
                 <Button
                   onClick={addNewColumn}
-                  variant="contained"
-                  color="success"
-                  size="small"
+                  variant='contained'
+                  color='success'
+                  size='small'
                   sx={{
-                    boxShadow: "none",
-                    border: "0.5px solid",
+                    boxShadow: 'none',
+                    border: '0.5px solid',
                     borderColor: (theme) => theme.palette.success.main,
-                    "&:hover": { bgcolor: (theme) => theme.palette.success.main },
+                    '&:hover': { bgcolor: (theme) => theme.palette.success.main },
                   }}
                 >
                   Add Column
                 </Button>
               </Tooltip>
-              <Tooltip title="Close">
+              <Tooltip title='Close'>
                 <CloseIcon
-                  fontSize="small"
+                  fontSize='small'
                   sx={{
-                    color: "white",
-                    cursor: "pointer",
-                    "&:hover": {
+                    color: 'white',
+                    cursor: 'pointer',
+                    '&:hover': {
                       color: (theme) => theme.palette.success.main,
                     },
                   }}
